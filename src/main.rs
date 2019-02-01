49e8c4535d15b5914e6c520ca6cbec149c50ad77
Federico Miras <fefo@decentraland.org>
2019-02-01 01:53:57 -0300

    feat: generate vanity address case insensitive

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,21 @@ extern crate ethkey;
 extern crate hex;
 extern crate rand;
 
-use ethkey::{Error, KeyPair, Secret};
+use ethkey::{KeyPair, Secret};
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
 
 fn main() {
-  let random32char: String = thread_rng().sample_iter(&Alphanumeric).take(32).collect();
-  let secret = Secret::from_slice(random32char.as_bytes());
-  println!("New secret: {:? }", secret);
-  let kp = KeyPair::from_secret(secret.unwrap());
-  println!("New public: {:? }", kp.unwrap().address());
+  let mut done: bool = false;
+  while !done {
+    let random32char: String = thread_rng().sample_iter(&Alphanumeric).take(32).collect();
+    let secret = Secret::from_slice(random32char.as_bytes());
+    println!("New secret: {:? }", secret);
+    let kp = KeyPair::from_secret(secret.unwrap());
+    let address = kp.unwrap().address();
+    println!("New public: {:? }", address);
+    if address.to_string().contains("fefe") {
+      done = true;
+    }
+  }
 }
